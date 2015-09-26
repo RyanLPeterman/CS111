b095f1b6986ecd6358d771b5fa3cb5007391a7b7
RyanLPeterman <RyanLPeterman@gmail.com>
2015-09-26 00:00:53 -0700

    Added the operator and command stack data structures in read-command.c

--- Lab 1/read-command.c
+++ Lab 1/read-command.c
@@ -11,14 +11,93 @@
 /* FIXME: Define the type 'struct command_stream' here.  This should
    complete the incomplete type declaration in command.h.  */
 
+typedef struct stack_t
+{
+  int m_count;
+  node* m_head;
+  node* m_tail;
+  bool is_command_stack;
+
+  typedef struct node {
+    void* m_dataptr;
+    node* m_next;
+  };
+};
+
+// TODO: is this dangerous programming?
+static stack_t* command_stack;
+static stack_t* operator_stack;
+command_stack->is_command_stack = true;
+operator_stack->is_command_stack = false;
+
+void push(void* to_push) {
+
+  // TODO: is there a way to remove command_stack in front of 
+  // each of the variables?
+
+  // empty stack
+  if (command_stack->m_head == null) {
+    // Allocating memory for node
+    command_stack->m_head = (node*) malloc(sizeof(node)));
+
+    // Fill in node and update head & tail
+    command_stack->m_head->m_next = null;
+    command_stack->m_head->m_dataptr = to_push;
+    command_stack->m_head = command_stack->m_tail;
+  }
+  else {
+    // Allocating memory for node
+    command_stack->m_tail->m_next = (node*) malloc(sizeof(node)));
+    
+    // Fill in node and update tail
+    command_stack->m_tail->m_dataptr = to_push;
+    command_stack->m_tail->m_next = null;
+    command_stack->m_tail = command_stack->m_tail->m_next;
+  }
+
+  command_stack->m_count++;
+
+  return;
+}
+
+void* pop() {
+
+  if(command_stack->m_head == null){
+    error(1, 0, "cannot pop, stack is empty");
+    return;
+  }
+
+  void* to_return = command_stack->m_tail->m_dataptr; 
+
+  // free the last node on the stack
+  free(command_stack->m_tail);
+
+  command_stack->m_count--;
+
+  // restore the tail pointer to the end of the stack
+  for(int i = 0; i < (command_stack->m_count - 1); i++) {
+    command_stack->m_tail = command_stack->m_head;
+
+    command_stack->m_tail = command_stack->m_tail->m_next;
+  }
+
+  return to_return;
+}
+
+
 command_stream_t
 make_command_stream (int (*get_next_byte) (void *),
 		     void *get_next_byte_argument)
 {
-  /* FIXME: Replace this with your implementation.  You may need to
-     add auxiliary functions and otherwise modify the source code.
-     You can also use external functions defined in the GNU C Library.  */
-  error (1, 0, "command reading not yet implemented");
+  /* Method: TODO
+            
+
+  */
+  
+  char next_byte = (*get_next_byte)(get_next_byte_argument);
+
+
+  error (1, 0, "command reading not yet implemented1");
   return 0;
 }
 
@@ -26,6 +105,6 @@ command_t
 read_command_stream (command_stream_t s)
 {
   /* FIXME: Replace this with your implementation too.  */
-  error (1, 0, "command reading not yet implemented");
+  error (1, 0, "command reading not yet implemented2");
   return 0;
 }
